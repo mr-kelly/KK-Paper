0cb25f8256a58c30375db724ab07496ebf29714e
Mrkelly <Mrkelly@Chan-KellymatoMacBook.local>
2010-12-13 19:30:50 +0800

    主题style.css

--- templates/kk_default/header.php
+++ templates/kk_default/header.php
@@ -1,3 +1,17 @@
-<?php
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+        "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+	<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+		
+	<link rel="stylesheet" type="text/css" href="<?=static_url('../templates/' . KK_THEME . '/style.css');?>">
+	<script type="text/javascript" src="/js/jquery.js?1291987673"></script>
 
-	// Header
+	<title><?= ( isset($page_title) ) ? $page_title : 'KK后台管理';?></title>
+	
+	
+</head>
+<body>
+
+</body>
+</html>
